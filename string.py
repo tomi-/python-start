820c22cc477bc7f2e24b431c37e46cd6ffce71f5
tomi- <grabcannon@gmail.com>
2017-01-20 19:00:05 +0900

    string

--- string.py
+++ string.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 print('some string')
 print('I\'m lovin\' it')
+print('C:\some\name')
+print(r'C:\some\name')
+
 print("I'm lovin' it")
 print('some\n string')
 print("some\n string")
