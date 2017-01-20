10982e097b2f04995d6e4123aba61f88aed14b99
tomi- <grabcannon@gmail.com>
2017-01-20 18:57:17 +0900

    syntax

---
+++ syntax.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+print(3*'never,'+'give up.')
+print(3*('never,'+'give up.'))
+
+fir = 'Py'
+integer = 334
+print('Py''thon')
+print(fir+'thon')
+# print(fir+integer) string+int は連結出来ない
+
+python = 'Python'
+print(python[0])
+print(python[-1])
+print(python[1:3])
+print(python[:3])
+print(python[:-3])
+print(python[3:])
+print(python[-3:])
+
+print('J'+python[1:])
+print(python[0:2]*2)
+print(len(python))
